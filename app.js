8bc498d7f94cb57f9b29f3f7ddde16f597916fad
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-08-31 19:58:15 -0300

    Create files app.js and server.js
    Tracking-Id: DM124

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/tasks', require('./api/routes/tasks'));
+app.use('/api/users', require('./api/routes/users'));
+
+app.use(require('./api/middleware/not-found'));
+
+module.exports = app;
