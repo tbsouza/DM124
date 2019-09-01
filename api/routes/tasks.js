7f20097d6c06081c524560f9ce1f37ee3756c01b
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-09-01 11:38:57 -0300

    Update the class tasks and the Rest operations
    Tracking-Id: DM124

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,55 +1,75 @@
+import '../models/Entrega';
+
 const express = require('express');
 const router = express.Router();
 
 const checkAuth = require('../middleware/check-auth');
 const notFound = require('../middleware/not-found');
 
+// List of all 'Entrega' saved
 let db = {};
-let sequence = 0;
 
+// POST: Create a 'Entrega'
 router.post('/', checkAuth, (request, response) => {
-  const newTask = {
-    id: ++sequence,
-    done: request.body.done || false,
-    description: request.body.description
-  };
+  
+  const idPedido = request.body.idPedido;
+  const idCliente = request.body.idCliente;
+  const nomeRecebedor = request.body.nomeRecebedor;
+  const cpfRecebedor = request.body.cpfRecebedor;
+  const isComprador = request.body.isComprador;
+  const dataHoraEntrega = request.body.dataHoraEntrega;
+  const localização = request.body.localização;
 
-  db[newTask.id] = newTask;
+  // Crete new 'Entrega'
+  const entrega = new Entrega(idPedido, idCliente, nomeRecebedor,
+        cpfRecebedor, isComprador, dataHoraEntrega, localização);
+  
+  // Add the 'Entrega' in the list
+  db[idPedido] = entrega;
 
   response.status(201).json(newTask);
 });
 
+//GET: Retrieve all 'Entrega'
 router.get('/', (request, response) => {
   const toArray = key => db[key];
-  const tasks = Object.keys(db).map(toArray);
-  tasks && tasks.length
-    ? response.json(tasks)
+  const entregas = Object.keys(db).map(toArray);
+  entregas && entregas.length
+    ? response.json(entregas)
     : response.status(204).end();
 });
 
-router.get('/:taskId', (request, response) => {
-  const task = db[request.params.taskId];
-  task
-    ? response.json(task)
+//GET: Retrieve a 'Entrega' by id
+router.get('/:idPedido', (request, response) => {
+  const entrega = db[request.params.idPedido];
+  entrega
+    ? response.json(entrega)
     : notFound(request, response);
 });
 
-router.patch('/:taskId', checkAuth, (request, response) => {
-  const task = db[request.params.taskId];
-  const hasValue = request.body.done != null
-  if(task) {
-    task.done = hasValue ? request.body.done : task.done;
-    task.description = request.body.description || task.description;
-    response.json(task);
+//PATCH: Update a 'Entrega'
+router.patch('/:idPedido', checkAuth, (request, response) => {
+  const entrega = db[request.params.idPedido];
+
+  if(entrega) {
+    entrega.idCliente = request.body.idCliente || entrega.idCliente;
+    entrega.nomeRecebedor = request.body.nomeRecebedor || entrega.nomeRecebedor;
+    entrega.cpfRecebedor = request.body.cpfRecebedor || entrega.cpfRecebedor;
+    entrega.isComprador = request.body.isComprador || entrega.isComprador;
+    entrega.dataHoraEntrega = request.body.dataHoraEntrega || entrega.dataHoraEntrega;
+    entrega.localização = request.body.localização || entrega.localização;
+
+    response.json(entrega);
   } else {
     notFound(request, response);
   }
 });
 
-router.delete('/:taskId', checkAuth, (request, response) => {
-  const task = db[request.params.taskId];
-  if(task) {
-    delete db[task.id];
+//DELETE: Delete a 'Entrega'
+router.delete('/:idPedido', checkAuth, (request, response) => {
+  const entrega = db[request.params.idPedido];
+  if(entrega) {
+    delete db[entrega.idPedido];
     response.status(200).end();
   } else {
     notFound(request, response);
